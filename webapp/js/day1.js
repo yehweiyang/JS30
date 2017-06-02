f137e44fddf14d20daeb92ee9b474d45604d63f3
BIBBY <tw00182691@gmail.com>
2017-06-02 18:41:14 +0800

    temp

--- webapp/js/day1.js
+++ webapp/js/day1.js
@@ -1,7 +1,6 @@
 console.log(66666666666666666)
 
 window.addEventListener('keydown',function(e){
-	console.log(document.querySelector('.btn'));
-    if(e.keyCode!=65||e.keyCode!=83||e.keyCode!=68||e.keyCode!=70)return
-	console.log(e.keyCode);
+	console.log(document.querySelector('btn'));
+
 })
