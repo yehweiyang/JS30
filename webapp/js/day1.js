7dbd6f3685a8e222cacc6c3daf3af45947a26b59
yehweiyang <tw00182691@gmail.com>
2017-06-05 08:37:01 +0800

    初版完成

--- webapp/js/day1.js
+++ webapp/js/day1.js
@@ -1,6 +1,35 @@
-console.log(66666666666666666)
 
 window.addEventListener('keydown',function(e){
-	console.log(document.querySelector('btn'));
 
+	const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+	const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+	if(!audio)return;
+	audio.play();
+	audio.currentTime=0;
+	key.classList.add("button");
+
+
+const keys = document.querySelectorAll(".key");
+
+//console.log(keys);
+
+keys.forEach(function(element,index){
+
+setTimeout(function(){
+element.classList.remove('button')
+ }, 400);
+
+	
 })
+
+keys.forEach(key=>addEventListener('transitionend',function(e){
+console.log(e)
+}))
+
+
+});
+
+// window.addEventListener("animationend", function(e){
+// 	console.log(e)
+// 	console.log('test');
+// });
